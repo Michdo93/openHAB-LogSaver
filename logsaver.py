a43534e546adc3239908e3dc4c0ce761d2c9aeed
Michdo93 <michaeldoerflinger93@gmail.com>
2022-03-09 13:46:47 +0100

    Update logsaver.py

--- logsaver.py
+++ logsaver.py
@@ -4,6 +4,7 @@ from os import SEEK_END
 import sys
 import mariadb
 from threading import Thread
+import time
 
 class OpenHABLogReader(object):
 
@@ -44,26 +45,44 @@ class OpenHABLogReader(object):
 
     def createTable(self, tablename):
         try:
-            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {tablename} (id INT NOT NULL AUTO_INCREMENT, datetime DATETIME, log_level VARCHAR(10), log_event VARCHAR(30), log_message LONGTEXT, CONSTRAINT AvoidTwiceLogsConstraint UNIQUE (datetime,log_level,log_event,log_message), PRIMARY KEY(id)) ENGINE=InnoDB DEFAULT CHARSET=latin1")
+            self.cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {tablename} (id INT NOT NULL AUTO_INCREMENT, datetime DATETIME, log_level VARCHAR(10), log_event VARCHAR(30), log_message LONGTEXT, CONSTRAINT AvoidTwiceLogsConstraint UNIQUE (datetime,log_level,log_event,log_message), PRIMARY KEY(id)) ENGINE=InnoDB DEFAULT CHARSET=latin1")
         except mariadb.Error as e:
             print(f"Error creating Table: {e}")
 
     def readAndSaveLogFile(self, file, tablename):
+        while not os.path.exists(self.location + file):
+            time.sleep(1)
+
         for filename in os.listdir(self.location):
             if filename == file:
                 while True:
+                    while not os.path.exists(self.location + file):
+                        time.sleep(1)
+                        continue
                     self.stamp = os.stat(self.location + file).st_mtime
+                    if os.stat(self.location + file).st_size == 0:
+                        time.sleep(1)
+                        continue
                     if self.stamp > self._cached_stamp:
                         self._cached_stamp = self.stamp
                         f = open(os.path.join(self.location, file), "r")
-                        self.last_log = f.readlines()[-1]
+
+                        read = f.readlines()
+
+                        if len(read) < 0:
+                            time.sleep(1)
+                            continue
+
+                        self.last_log = read[-1]
 
                         print(self.last_log)
 
                         splitted = self.last_log.replace(" ]", "]").split()
                         datetime = splitted[0] + " " + splitted[1]
                         log_level = splitted[2][1:].replace("]", "")
-                        log_event = splitted[3].rsplit(".", 1)[-1].replace("]", "")
+                        log_event = splitted[3].rsplit(
+                            ".", 1)[-1].replace("]", "")
                         log_message = ""
 
                         for i in range(5, len(splitted), 1):
@@ -90,13 +109,14 @@ class OpenHABLogReader(object):
                         except mariadb.Error as e:
                             print(f"Error commiting insert values: {e}")
 
+
 if __name__ == "__main__":
     reader = OpenHABLogReader()
 
-    reader.connect("", "", "", 3306, "OpenHAB_LOGS")
+    reader.connect("openhab", "gHSi5%64D", "127.0.0.1", 3306, "OpenHAB_LOGS")
     reader.createTable("logs")
 
-    Thread(target = reader.readAndSaveLogFile("events.log", "logs")).start()
-    Thread(target = reader.readAndSaveLogFile("openhab.log", "logs")).start()
+    Thread(target=reader.readAndSaveLogFile("events.log", "logs")).start()
+    Thread(target=reader.readAndSaveLogFile("openhab.log", "logs")).start()
 
     reader.disconnect()
